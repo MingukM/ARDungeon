ad079fb4d942afe24c6bb1f3ffc157f0c97ad5bf
Minguk Moon <32960976+MingukM@users.noreply.github.com>
2018-04-19 16:19:52 -0400

    Add files via upload

---
+++ UI/InGameMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+
+	// Allow palyer to go to dungeon by pressing a button on the screen
+	public void GoToDungeon(){
+		SceneManager.LoadScene ("Dungeon");
+	}
+}
---
+++ UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// When player start game it opens up the base camp
+	public void PlayGame(){
+		SceneManager.LoadScene ("Base");
+	}
+
+	// Allow player to quit game
+	public void QuitGame() {
+		Debug.Log ("QUIT");
+		Application.Quit();
+	}
+}
