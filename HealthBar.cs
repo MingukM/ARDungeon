ffac9765c1bc921daf19f71dee79b7c5b356a2ad
ssucci12 <35583020+ssucci12@users.noreply.github.com>
2018-04-25 18:28:13 -0400

    Add files via upload

---
+++ HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class HealthBar : MonoBehaviour {
+
+    Camera mainCamera;
+
+    public Image healthBarImage;
+    public Entity entity;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Sets the fill amount
+        healthBarImage.fillAmount = (float)entity.health / 100;
+
+        // Rotates the healthbar to face the camera
+        transform.rotation = Quaternion.LookRotation(-mainCamera.transform.forward);
+
+	}
+}
---
+++ RandomMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class RandomMovement : MonoBehaviour
+{
+
+
+    public float minSpeed = 3.5f;  // minimum range of speed to move
+    public float maxSpeed = 5f;  // maximum range of speed to move
+
+    public float minDistance = 1f;
+    public float maxDistance = 10f;
+    public float triggerDistance = 0.5f;
+
+    NavMeshAgent agent;
+
+    Vector3 waypoint;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = GetRandomSpeed();
+
+        waypoint = transform.position; 
+    }
+
+    private void Update()
+    {
+        // Set a new waypoint if the monster gets close to the current one
+        if(Vector3.Distance(transform.position, agent.pathEndPosition) < triggerDistance)
+        {
+            waypoint = GetRandomPosition() * Random.Range(minDistance, maxDistance);
+
+            agent.SetDestination(waypoint);
+
+            agent.speed = Random.Range(minSpeed, maxSpeed);
+        }
+
+
+        Debug.DrawLine(transform.position, agent.pathEndPosition, Color.yellow);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        return new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+    }
+
+    private float GetRandomSpeed()
+    {
+        return Random.Range(minSpeed, maxSpeed);
+    }
+
+}
